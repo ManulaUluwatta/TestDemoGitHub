1dfe2d2f9319ee84e5b3f31ddd346faf5182b416
ManulaUluwatta <manulau97@gmail.com>
2020-04-01 02:05:52 +0530

    Add sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         MyClass myObject=new MyClass();
         System.out.println(myObject.addNumber(3,5));
+        System.out.println("Hello Manula Uluwatta");
     }
 }
