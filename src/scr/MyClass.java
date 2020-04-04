d34bde73ce3a08ca8e504a2078183da97e27ae58
ManulaUluwatta <manulau97@gmail.com>
2020-04-04 22:45:37 +0530

    add findPrimeNumber method into MyClass

--- src/scr/MyClass.java
+++ src/scr/MyClass.java
@@ -4,6 +4,21 @@ public class MyClass {
     public int addNumber(int a, int b){
         return a+b;
     }
+    public int calculateMaxValue(int x,int y,int z){
+        int max=0;
+        if(x>max){
+            max=x;
+        }
+        if(y>max){
+            max=y;
+        }
+        if (z>max){
+            max=z;
+        }
+        return max;
+
+
+    }
     public boolean findPrimeNumber(int a){
         int value=a;
         for(int i=1;i<=value;i++){
